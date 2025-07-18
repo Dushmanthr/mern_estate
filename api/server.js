c6b6cd4db5c37ecc99f2a2aa9fb027cd0256f22d
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2025-07-18 10:45:51 +0530

    feat: database connection

--- api/server.js
+++ api/server.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_CONNECTION).then(()=>{
+    console.log('Database connected successfully');
+}).catch((err)=>{
+    console.log("Database connection failed", err);
+})
 
 const app = express();
 
