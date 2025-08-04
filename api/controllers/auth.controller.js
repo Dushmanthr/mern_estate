62dc242e6cb437952ae3033f8dbce2bb77b6db7d
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2025-08-04 10:35:21 +0530

    feat: create sign in API completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,7 +31,8 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(401, "Invalid Credentials"));
     }
     const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
-    res.cookie('access token', token, {httpOnly: true}).status(200).json(validUser)
+    const {password: pass, ...rest} = validUser._doc;
+    res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest)
   }
   catch(error){
     next(error);
